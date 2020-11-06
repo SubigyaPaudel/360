76165548790e4f5162939758d4da5b082aa152d6
Amartya Vats <amartyavats@Amartyas-MacBook-Pro.local>
2020-11-06 20:46:19 +0100

    -v

---
+++ public_html/cookies.php
@@ -0,0 +1,16 @@
+<?php include('./header.php'); ?>
+
+<?php 
+    $cookie_name = "user";
+    $cookie_value = "Alex Porter";
+    setcookie($cookie_name, $cookie_value, time() + (900));
+
+    if(!isset($_COOKIE[$cookie_name])) {
+        echo "Cookie named '" . $cookie_name . "' is not set!";
+    } else {
+        echo "Cookie '" . $cookie_name . "' is set!<br>";
+        echo "Value is: " . $_COOKIE[$cookie_name];
+    }
+?>
+
+<?php include('./footer.php'); ?>
